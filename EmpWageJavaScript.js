95aabb597a8af12aa8247011ee2620629e44c6e6
sharad2512 <shnarute95@gmail.com>
2023-02-04 18:36:01 +0530

    ADDED : Code to calculate wages for a Month

--- EmpWageJavaScript.js
+++ EmpWageJavaScript.js
@@ -4,8 +4,9 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HURS = 8;
 const WAGE_PER_HOURS = 20;
-let empCheck = Math.floor(Math.random() * 10) % 3;
+const NUM_OF_WORKING_DAYS = 2;
 let empHrs = 0;
+function getWorkingHours(empCheck){
 switch(empCheck){
     case IS_FULL_TIME:
         empHrs = IS_FULL_TIME;
@@ -14,7 +15,12 @@ switch(empCheck){
         empHrs = IS_PART_TIME;
         break;
     default:
-        empHrs = 0;        
+        empHrs = 0;      
+}
+}
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = empHrs + getWorkingHours(empCheck);
 }
 let employeeWage = empHrs * WAGE_PER_HOURS;
-console.log("Employee Wage "+employeeWage);
+console.log("Total Hours:"+empHrs+"Employee wage"+employeeWage);
