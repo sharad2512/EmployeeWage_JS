42e08eb50a2229a446ba9a57a7e519c6b683d574
sharad2512 <shnarute95@gmail.com>
2023-02-08 11:16:22 +0530

    Calculate Wages Till Number OF Working Days Or Total Working Hours Per Month Reached

--- EmpWageJavaScript.js
+++ EmpWageJavaScript.js
@@ -1,10 +1,13 @@
 const IS_EMP_ABCENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
+const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 10;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HURS = 8;
 const WAGE_PER_HOURS = 20;
-const NUM_OF_WORKING_DAYS = 2;
 let empHrs = 0;
 function getWorkingHours(empCheck){
 switch(empCheck){
@@ -16,11 +19,11 @@ switch(empCheck){
         break;
     default:
         empHrs = 0;      
-}
-}
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+}}
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs = empHrs + getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
-let employeeWage = empHrs * WAGE_PER_HOURS;
-console.log("Total Hours:"+empHrs+"Employee wage"+employeeWage);
+let employeeWage = totalEmpHrs * WAGE_PER_HOURS;
+console.log("Tatal Days"+ totalWorkingDays+"Employee Wage"+employeeWage+"Total Hrs"+totalEmpHrs);
